5aaa3ff57477c2f24bbd9a0928de0237ff7e23ed
Aslamsidheeq <aslamsidhik22@gmail.com>
2023-08-18 11:40:44 +0530

    1

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+require('./db/coonect')
 const express = require('express');
 const app = express();                      //initialising
+const tasks = require('./routes/tasks')
+
+//middleware
+app.use(express.json())
 
 //routes
 app.get('/hello',(req,res)=>{
     res.send('Task Manager App')
 })
-
+app.use('/api/v1/tasks',tasks)              //using middleware
 
 const port = 3000
 app.listen(port,console.log(`Server is listening on port ${port}`))
