00e71c90b0ff3fbbf4d114a04d2b794fb20d0fcd
Aslamsidheeq <aslamsidhik22@gmail.com>
2023-08-21 13:03:56 +0530

    env updated

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-require('./db/coonect')
 const express = require('express');
 const app = express();                      //initialising
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
 
 //middleware
 app.use(express.json())
@@ -13,4 +14,14 @@ app.get('/hello',(req,res)=>{
 app.use('/api/v1/tasks',tasks)              //using middleware
 
 const port = 3000
-app.listen(port,console.log(`Server is listening on port ${port}`))
+
+const start = async () =>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,console.log(`Server is listening on port ${port}`))
+
+    }catch(error){
+        console.log(error)
+    }
+}
+start()
