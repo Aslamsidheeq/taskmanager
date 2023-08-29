db00759dbb52745d5b8db07c50ff3c16fc4ca53a
Aslamsidheeq <aslamsidhik22@gmail.com>
2023-08-29 14:46:19 +0530

    final

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -15,7 +15,6 @@ const createTask = async (req,res)=>{
     } catch(error){
         res.status(500).json({msg:error})
     }
-    
 }
 const getTask = async (req,res)=>{
     try{
@@ -27,7 +26,7 @@ const getTask = async (req,res)=>{
         }
         res.status(200).json({task})
     }catch(error){ //if no value/syntax error
-        res.status(500).json({msg:"wat the hell",error})
+        res.status(500).json({msg:error})
     }
 }
 
@@ -36,15 +35,16 @@ const updateTask =async(req,res)=>{
         const {id:taskID} = req.params;
         const task = await Task.findByIdAndUpdate({_id:taskID},req.body,{
             new:true,
-            runValidators:true
+            runValidators:true,
+            overwrite:true
         })
         if(!task){
             return res.satus(404).json({msg: `No task found with the id : ${taskID}` })
         }
         res.status(200).json({task})
 
-    }catch(error){
-        res.status(500).json({msg:"wat the hell",error})
+        }catch(error){
+        res.status(500).json({msg:error})
     }
 }
 
@@ -59,7 +59,6 @@ const deleteTask = async (req,res)=>{
     }catch(error){
         res.status(404).json({msg:error})
     }
-
 }
 
 module.exports={
